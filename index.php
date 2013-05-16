0d3583a18250af592f83e0ccba2922fd39cd956d
ZoM <pkhantidhara@zinzone.net>
2013-05-15 21:59:54 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+
+    <head>
+        <title>Bootstrap 101 Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+        <!--========================== [START] LESS ==========================-->
+        <link rel="stylesheet/less" type="text/css" href="/css/index.less" />
+        <!--========================== [END] LESS ==========================-->
+
+
+        <!--========================== [START] JS imports ==========================-->
+        <script src="/lib/less.min.js"></script>
+        <!--========================== [END] JS imports ==========================-->
+
+        <link href="/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    </head>
+
+    <body>
+
+        <div>
+
+        </div>
+
+        <!--========================== [START] JS imports ==========================-->
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script src="/lib/bootstrap/js/bootstrap.min.js"></script>
+        <!--========================== [END] JS imports ==========================-->
+    </body>
+
+</html>
+
